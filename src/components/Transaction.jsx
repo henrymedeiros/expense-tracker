b2f71cd2d690632b4d8438868fb05394363b05c6
henrymedeiros <henrymedeiros77@gmail.com>
2022-09-03 01:09:35 -0300

    Parcial editing function

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,15 +1,97 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { RiEdit2Fill, RiDeleteBin6Fill } from "react-icons/ri";
 
 const Transaction = ({ transaction }) => {
-  const { deleteTransactions } = useContext(GlobalContext);
+  const { deleteTransactions, updateTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? "-" : "+";
+  const [editMode, setEditMode] = useState(false);
+  const [transactionText, setTransactionText] = useState(transaction.text);
+  const [transactionAmount, setTransactionAmount] = useState(
+    transaction.amount
+  );
+
+  const onEdit = () => {
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text: "edit",
+      amount: 123,
+    };
+
+    updateTransaction(transaction.id, newTransaction);
+  };
+
   return (
     <>
       <li className={sign === "-" ? "minus" : "plus"}>
-        {transaction.text}{" "}
-        <span>
-          {sign} ${Math.abs(transaction.amount)}
+        <span
+          onDoubleClick={() => {
+            if (editMode) {
+              const updatedTransaction = {
+                id: transaction.id,
+                text: transactionText,
+                amount: transaction.amount,
+              };
+
+              updateTransaction(transaction.id, updatedTransaction);
+              setEditMode(false);
+            } else {
+              setEditMode(true);
+            }
+          }}
+        >
+          {editMode ? (
+            <input
+              type="text"
+              value={transactionText}
+              onChange={(e) => setTransactionText(e.target.value)}
+              placeholder="edit mode"
+            />
+          ) : (
+            transactionText
+          )}
+        </span>
+        <span
+          onDoubleClick={() => {
+            if (editMode) {
+              const updatedTransaction = {
+                id: transaction.id,
+                text: transaction.text,
+                amount: +transactionAmount,
+              };
+
+              updateTransaction(transaction.id, updatedTransaction);
+              setEditMode(false);
+            } else {
+              setEditMode(true);
+            }
+          }}
+        >
+          {editMode ? (
+            <input
+              type="text"
+              value={transactionAmount}
+              onChange={(e) => setTransactionAmount(e.target.value)}
+              placeholder="edit mode"
+            />
+          ) : (
+            <div>
+              <RiEdit2Fill
+                onClick={() => {
+                  if (editMode) {
+                    setEditMode(false);
+                  } else {
+                    setEditMode(true);
+                  }
+                }}
+              ></RiEdit2Fill>
+              <RiDeleteBin6Fill></RiDeleteBin6Fill>
+
+              <span>
+                {sign} ${Math.abs(transaction.amount)}{" "}
+              </span>
+            </div>
+          )}
         </span>
         <button
           className="delete-btn"
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,30 +1,11 @@
 import { createContext, useReducer } from "react";
-
-const AppReducer = (state, action) => {
-  switch (action.type) {
-    case "DELETE_TRANSACTION":
-      return {
-        ...state,
-        transactions: state.transactions.filter(
-          (transaction) => transaction.id !== action.payload
-        ),
-      };
-    case "ADD_TRANSACTION":
-      return {
-        ...state,
-        transactions: [...state.transactions, action.payload],
-      };
-    default:
-      return state;
-  }
-};
-
+import { v4 as uuidv4 } from "uuid";
 const initialState = {
   transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
+    { id: uuidv4(), text: "Flower", amount: -20 },
+    { id: uuidv4(), text: "Salary", amount: 300 },
+    { id: uuidv4(), text: "Book", amount: -10 },
+    { id: uuidv4(), text: "Camera", amount: 150 },
   ],
 };
 
@@ -48,17 +29,53 @@ const GlobalProvider = ({ children }) => {
     });
   }
 
+  function updateTransaction(id, newTransaction) {
+    dispatch({
+      type: "UPDATE_TRANSACTION",
+      payload: { id, newTransaction },
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
         deleteTransactions,
         addTransaction,
+        updateTransaction,
       }}
     >
       {children}
     </GlobalContext.Provider>
   );
+};
+
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
+    case "UPDATE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.map((transaction) =>
+          transaction.id === action.payload.id
+            ? action.payload.newTransaction
+            : transaction
+        ),
+      };
+    default:
+      return state;
+  }
 };
 
 export default GlobalProvider;
