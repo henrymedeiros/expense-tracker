911d8813cb2624cc72d398499e32aede7eb3ffaa
henrymedeiros <henrymedeiros77@gmail.com>
2022-09-04 21:46:27 -0300

    edit feat 100%

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -6,93 +6,49 @@ const Transaction = ({ transaction }) => {
   const { deleteTransactions, updateTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? "-" : "+";
   const [editMode, setEditMode] = useState(false);
+
   const [transactionText, setTransactionText] = useState(transaction.text);
   const [transactionAmount, setTransactionAmount] = useState(
     transaction.amount
   );
 
-  const onEdit = () => {
-    const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
-      text: "edit",
-      amount: 123,
-    };
-
-    updateTransaction(transaction.id, newTransaction);
-  };
-
   return (
     <>
       <li className={sign === "-" ? "minus" : "plus"}>
-        <span
-          onDoubleClick={() => {
-            if (editMode) {
-              const updatedTransaction = {
-                id: transaction.id,
-                text: transactionText,
-                amount: transaction.amount,
-              };
-
-              updateTransaction(transaction.id, updatedTransaction);
-              setEditMode(false);
-            } else {
-              setEditMode(true);
-            }
-          }}
-        >
-          {editMode ? (
+        {editMode ? (
+          <div
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === "Escape") {
+                updateTransaction(transaction.id, {
+                  id: transaction.id,
+                  text: transactionText,
+                  amount: +transactionAmount,
+                });
+                setEditMode(false);
+              }
+            }}
+          >
             <input
+              autoFocus
               type="text"
               value={transactionText}
               onChange={(e) => setTransactionText(e.target.value)}
-              placeholder="edit mode"
             />
-          ) : (
-            transactionText
-          )}
-        </span>
-        <span
-          onDoubleClick={() => {
-            if (editMode) {
-              const updatedTransaction = {
-                id: transaction.id,
-                text: transaction.text,
-                amount: +transactionAmount,
-              };
-
-              updateTransaction(transaction.id, updatedTransaction);
-              setEditMode(false);
-            } else {
-              setEditMode(true);
-            }
-          }}
-        >
-          {editMode ? (
             <input
-              type="text"
+              type="number"
               value={transactionAmount}
               onChange={(e) => setTransactionAmount(e.target.value)}
-              placeholder="edit mode"
             />
-          ) : (
-            <div>
-              <RiEdit2Fill
-                onClick={() => {
-                  if (editMode) {
-                    setEditMode(false);
-                  } else {
-                    setEditMode(true);
-                  }
-                }}
-              ></RiEdit2Fill>
-              <RiDeleteBin6Fill></RiDeleteBin6Fill>
+          </div>
+        ) : (
+          <div onClick={() => setEditMode(true)}>
+            <span>{transactionText}</span>
+            <span>
+              {sign} ${Math.abs(transactionAmount)}{" "}
+            </span>
+          </div>
+        )}
 
-              <span>
-                {sign} ${Math.abs(transaction.amount)}{" "}
-              </span>
-            </div>
-          )}
-        </span>
         <button
           className="delete-btn"
           onClick={() => deleteTransactions(transaction.id)}
